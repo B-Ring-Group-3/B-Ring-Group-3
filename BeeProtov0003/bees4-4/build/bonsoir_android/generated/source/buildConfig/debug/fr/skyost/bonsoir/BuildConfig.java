5500fc8bb7bafcf4cc6c9151284a11dabc2c5a26
Jon <nelsojo4@oregonstate.edu>
2024-02-22 17:41:38 -0800

    added version that more consistantly connects to bot on all screens, and a refresh button

---
+++ BeeProtov0003/bees4-4/build/bonsoir_android/generated/source/buildConfig/debug/fr/skyost/bonsoir/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package fr.skyost.bonsoir;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "fr.skyost.bonsoir";
+  public static final String BUILD_TYPE = "debug";
+}

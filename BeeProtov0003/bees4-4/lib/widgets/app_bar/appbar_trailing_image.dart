f99c6fec5ef95316fe98547b532940af87b354e5
Jon <nelsojo4@oregonstate.edu>
2024-02-22 17:55:50 -0800

    added version that more consistantly connects to bot on all screens, and a refresh button

---
+++ BeeProtov0003/bees4-4/lib/widgets/app_bar/appbar_trailing_image.dart
@@ -0,0 +1,36 @@
+import 'package:bees4/core/app_export.dart';
+import 'package:flutter/material.dart';
+
+// ignore: must_be_immutable
+class AppbarTrailingImage extends StatelessWidget {
+  AppbarTrailingImage({
+    Key? key,
+    this.imagePath,
+    this.margin,
+    this.onTap,
+  }) : super(
+          key: key,
+        );
+
+  String? imagePath;
+
+  EdgeInsetsGeometry? margin;
+
+  Function? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap != null ? () => onTap!.call() : null,
+      child: Padding(
+        padding: margin ?? EdgeInsets.zero,
+        child: CustomImageView(
+          imagePath: imagePath,
+          height: 32.v,
+          width: 33.h,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}

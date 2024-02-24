9acec9d73de514169cca3b253a138129a15a579d
Seongki Lee <leese5@oregonstate.edu>
2024-02-23 22:40:47 -0800

    google sign in fixed

--- BeeProtoV0001/bees4-4/lib/presentation/login_screen/login_screen.dart
+++ BeeProtoV0001/bees4-4/lib/presentation/login_screen/login_screen.dart
@@ -4,7 +4,7 @@ import 'package:bees4/widgets/custom_outlined_button.dart';
 import 'package:bees4/widgets/custom_text_form_field.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:flutterfire_ui/auth.dart';
 
 // ignore_for_file: must_be_immutable
@@ -64,4 +64,21 @@ class LoginScreen extends StatelessWidget {
       },
     );
   }
+}
+
+Future<UserCredential> signInWithGoogle() async {
+  // Trigger the authentication flow
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+
+  // Obtain the auth details from the request
+  final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
+
+  // Create a new credential
+  final OAuthCredential credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+
+  // Once signed in, return the UserCredential
+  return await FirebaseAuth.instance.signInWithCredential(credential);
 }
